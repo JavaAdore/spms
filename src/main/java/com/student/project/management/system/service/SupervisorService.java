acb7a49fbfd2cbbc5610f340eb55ca2aa981d5dc
mibrahim_iti <mibrahim@skios.com.mt>
2016-04-18 03:29:36 +0200

    new

---
+++ src/main/java/com/student/project/management/system/service/SupervisorService.java
@@ -0,0 +1,9 @@
+package com.student.project.management.system.service;
+
+import java.util.List;
+
+import com.student.project.management.system.entity.Supervisor;
+
+public interface SupervisorService {
+  List<Supervisor> findAll();
+}
