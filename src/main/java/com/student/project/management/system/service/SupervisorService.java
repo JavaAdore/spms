af33fd4f1326b24281606fba1ae8a6fed31cf383
mibrahim_iti <mibrahim@skios.com.mt>
2016-04-23 23:05:49 +0200

    changes

--- src/main/java/com/student/project/management/system/service/SupervisorService.java
+++
@@ -1,9 +0,0 @@
-package com.student.project.management.system.service;
-
-import java.util.List;
-
-import com.student.project.management.system.entity.Supervisor;
-
-public interface SupervisorService {
-  List<Supervisor> findAll();
-}
