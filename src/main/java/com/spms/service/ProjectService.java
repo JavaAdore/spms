13274d368e382655f74b3b76193cfe970f396b5a
mibrahim_iti <mibrahim@skios.com.mt>
2016-04-24 21:34:43 +0200

    new file

---
+++ src/main/java/com/spms/service/ProjectService.java
@@ -0,0 +1,19 @@
+package com.spms.service;
+
+import java.util.List;
+
+import com.spms.entity.Project;
+
+public interface ProjectService {
+
+	Project create(Project project);
+
+	Project find(Integer id);
+
+	Project update(Project project);
+
+	void delete(Project project);
+
+	List<Project> getAllProjects();
+
+}
---
+++ src/main/java/com/spms/serviceimpl/ProjectServiceImpl.java
@@ -0,0 +1,51 @@
+package com.spms.serviceimpl;
+
+import java.util.List;
+
+import javax.ejb.Stateless;
+import javax.ejb.TransactionAttribute;
+import javax.ejb.TransactionAttributeType;
+import javax.ejb.TransactionManagement;
+import javax.ejb.TransactionManagementType;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import com.spms.entity.Project;
+import com.spms.service.ProjectService;
+
+@Stateless
+@TransactionManagement(TransactionManagementType.CONTAINER)
+@TransactionAttribute(TransactionAttributeType.REQUIRED)
+public class ProjectServiceImpl implements ProjectService {
+
+	@PersistenceContext(unitName = "database")
+	EntityManager em;
+	
+	@Override
+	public Project create(Project project) {
+		em.persist(project);
+		return project;
+	}
+
+	@Override
+	public Project find(Integer id) {
+		return em.find(Project.class, id);
+	}
+
+	@Override
+	public Project update(Project project) {
+		return em.merge(project);
+	}
+
+	@Override
+	public void delete(Project project) {
+		em.remove(em.getReference(Project.class, project.getId()));
+	}
+
+	@SuppressWarnings("unchecked")
+	@Override
+	public List<Project> getAllProjects() {
+		return em.createNamedQuery("Project.findAll").getResultList();
+	}
+	
+}
