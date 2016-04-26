294aabf0f6a319b04755af4a4ff46e19c860de37
mibrahim_iti <mibrahim@skios.com.mt>
2016-04-26 02:56:55 +0200

    changes

--- src/main/java/com/spms/serviceimpl/SystemUserServiceImpl.java
+++ src/main/java/com/spms/serviceimpl/SystemUserServiceImpl.java
@@ -26,12 +26,7 @@ import com.spms.util.Util;
 @TransactionManagement(TransactionManagementType.CONTAINER)
 @TransactionAttribute(TransactionAttributeType.REQUIRED)
 public class SystemUserServiceImpl implements SystemUserService {
-
-	@PostConstruct
-	public void init() {
-
-	}
-
+	
 	@EJB
 	private SystemUserDAO systemUserDAO;
 
